0cc314fc3d885862745b9c8734cd439679b15a48
abolphp <abol99lb@gmail.com>
2025-10-01 13:26:13 +0330

    Make Category module

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     \Modules\User\Providers\UserServiceProvider::class,
+    \Modules\Category\app\Providers\CategoryServiceProvider::class,
 ];
